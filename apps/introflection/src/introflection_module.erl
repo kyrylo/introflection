a0a927631e3e9dfda22df29c8e5290455f91bcd8
Kyrylo Silin <silin@kyrylo.org>
2015-03-19 13:28:48 +0200

    Add introflection_module

---
+++ apps/introflection/src/introflection_module.erl
@@ -0,0 +1,52 @@
+-module(introflection_module).
+
+-include("mnesia_tables.hrl").
+-include("logger.hrl").
+
+
+%% API
+-export([init/1, install/1]).
+
+-export([add/4, find/1]).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+init(Nodes) ->
+    {atomic, ok} = mnesia:create_table(introflection_modules, [
+                       {type, set},
+                       {attributes, record_info(fields, introflection_modules)},
+                       {ram_copies, Nodes}]).
+
+install(Nodes) ->
+    ok = mnesia:create_schema(Nodes),
+    application:start(mnesia),
+    init(Nodes),
+    application:stop(mnesia).
+
+add(ObjectId, Name, Nesting, Parent) ->
+    F = fun() ->
+        mnesia:write(#introflection_modules{object_id=ObjectId,
+                                            name=Name,
+                                            nesting=Nesting,
+                                            parent=Parent})
+    end,
+    {atomic, ResultOfFun} = mnesia:transaction(F),
+    ResultOfFun.
+
+find(ObjectId) ->
+    F = fun() ->
+        case mnesia:read({introflection_modules, ObjectId}) of
+            [#introflection_modules{name=N, nesting=L, parent=P}] ->
+                {ObjectId, N, L, P};
+            [] ->
+                undefined
+        end
+    end,
+    {atomic, ResultOfFun} = mnesia:transaction(F),
+    ResultOfFun.
+
+%% find_all_modules() ->
+%%     F = fun() ->
+%%                 mnesia
